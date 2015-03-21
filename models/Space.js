31610cceadafe6aa86033bb39e04666be8fb34e5
Panos <pkokki@gmail.com>
2015-03-21 22:42:10 +0200

    space mongoose schema

--- models/Space.js
+++ models/Space.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
 
+var serviceDescriptor = {
+	uri: String,
+};
+
 var SpaceSchema = new mongoose.Schema({
 	name: String,
 	merchant_code: String,
@@ -7,21 +11,15 @@ var SpaceSchema = new mongoose.Schema({
 	description: String,
 	updated_at: { type: Date, default: Date.now },
 	database_type: { type: String, enum: ['mssql', 'oracle', 'sybase'], default: 'mssql' },
-	services: ServicesSchema,
-});
-
-var ServicesSchema = new mongoose.Schema({
-	org: ServiceDescriptorSchema,
-	cmis: ServiceDescriptorSchema,
-	cases: ServiceDescriptorSchema,
-	tasks: ServiceDescriptorSchema,
-	rules: ServiceDescriptorSchema,
-	domains: ServiceDescriptorSchema,
-	templates: ServiceDescriptorSchema,
-});
-
-var ServiceDescriptorSchema = new mongoose.Schema({
-	uri: String,
+	services: { type: {
+		org: { type: serviceDescriptor },
+		cmis: { type: serviceDescriptor },
+		cases: { type: serviceDescriptor },
+		tasks: { type: serviceDescriptor },
+		rules: { type: serviceDescriptor },
+		domains: { type: serviceDescriptor },
+		templates: { type: serviceDescriptor },
+	}},
 });
 
 module.exports = mongoose.model('Space', SpaceSchema);
